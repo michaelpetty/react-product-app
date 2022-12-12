b3834a021c9b841fa361579ee90c5e8aa17967d7
michaelpetty <michael.petty@gmail.com>
2022-12-11 23:14:15 -0800

    refactor class-based to function components w hooks

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,35 +1,41 @@
-import { Component } from "react"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
 import ProductCard from "../components/ProductCard"
 import ProductModel from "../models/product"
 
-class ProductDetail extends Component {
-
-    state = {
-        productId: 3,
-        product: {}
-    }
+const ProductDetail = () => {
+    // destructure the params obj and rename; assign the value for 
+    // the key id to the var productId
+    const {id:productId} = useParams()
+    const [product, setProduct] = useState({})
+    // state = {
+    //     productId: 3,
+    //     product: {}
+    // }
     
-    componentDidMount() {
-        ProductModel.show(this.state.productId)
+    useEffect(() => {
+        ProductModel.show(productId)
             .then((res) => {
                 console.log(res)
-                this.setState({product: res.data})
+                setProduct(res.data)
             })
-    }
+    }, [productId])
+    // componentDidMount() {
+    // }
 
-    render() {
-        return (
-            <>
-                <h2>Individual Product</h2>
-                {(this.state.product.title)? (
-                  <ProductCard product={this.state.product} detail/>
-                    ) : (
-                  <div>loading...</div>
-                  )}
-    
-            </>
+    // render() {
+    return (
+        <>
+            <h2>Individual Product</h2>
+            {(product.title)? (
+                <ProductCard product={product} detail/>
+                ) : (
+                <div>loading...</div>
+                )}
+
+        </>
         )
-    }
+    // }
 
 }
 
--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,58 +1,73 @@
-import { Component } from "react";
+// Update the import to include the useState AND useEffect hook; can rm Component
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button, Card, Dimmer, Loader, Segment } from "semantic-ui-react";
 import ProductCard from "../components/ProductCard";
 import ProductModel from "../models/product";
 
-class ProductList extends Component {
-    state = {
-        products: [],
-        maxDisplay: this.props.maxDisplay
-    }
+const ProductList = (props) => {
+    // state = {
+    //     products: [],
+    //     maxDisplay: this.props.maxDisplay
+    // }
+    const [products, setProducts] = useState([]) 
+    const [maxDisplay, setMaxDisplay] = useState(props.maxDisplay)
+    // useState always returns an array of two elements
+    // First element "games" is the value of the state returned
+    // Second element "setGames" is a setter function for updating the state
+    
+    // componentDidMount() {
+    //     this.fetchData()
+    // }
 
-    componentDidMount() {
-        this.fetchData()
-    }
+    // takes a callback function; REQUIRED first argument
+    // no second arg = called every render
+    // empty [] second arg = called once (componentDidMount)
+    // array with vars = called when one of those values changes
+    useEffect(() => {
+        fetchData()
+        // Return a "cleanup" function
+        return () => {
+            console.log("runs on unmount")
+        }
+    }, [])
 
-    loadMore = () => {
+    const loadMore = () => {
         console.log('button')
-        let maxDisplay = this.state.maxDisplay + 3
-        this.fetchData(maxDisplay)
+        // move setMaxDisplay to fetchData
+        fetchData(maxDisplay + 3)
     }
 
-    fetchData = (max=3) => {
+    const fetchData = (max=3) => {
         ProductModel.all(max)
             .then((res) => {
                 console.log(res)
-                this.setState({
-                    products: res.data,
-                    maxDisplay: max
-                })
+                setProducts(res.data)
+                setMaxDisplay(max)
             })
     }
 
-    render() {
-        let productCards = this.state.products.map((product) => (
-            <Link to={`/products/${product.id}`} key={product.id}>
-                <ProductCard product={product} key={product.id}/>
-            </Link>
-        ))
-        return (
-            <>
-                <h2>List of Products</h2>
-                <h3>Found: {this.state.products[0]? this.state.products.length:  '...Searching'}</h3>
-                <Segment style={{minHeight: "100px"}}>
-                        <Dimmer active={!this.state.products.length}>
-                            <Loader size='tiny' indeterminate>Getting Products</Loader>
-                        </Dimmer>
-                        <Card.Group centered stackable doubling itemsPerRow={3}>
-                            {productCards}
-                        </Card.Group>
-                </Segment>
-                <Button onClick={this.loadMore} content="Load More" color='black' fluid/>
-            </>
-        )
-    }
+    let productCards = products.map((product) => (
+        <Link to={`/products/${product.id}`} key={product.id}>
+            <ProductCard product={product} key={product.id}/>
+        </Link>
+    ))
+
+    return (
+        <>
+            <h2>List of Products</h2>
+            <h3>Found: {products[0]? products.length:  '...Searching'}</h3>
+            <Segment style={{minHeight: "100px"}}>
+                    <Dimmer active={!products.length}>
+                        <Loader size='tiny' indeterminate>Getting Products</Loader>
+                    </Dimmer>
+                    <Card.Group centered stackable doubling itemsPerRow={3}>
+                        {productCards}
+                    </Card.Group>
+            </Segment>
+            <Button onClick={loadMore} content="Load More" color='black' fluid/>
+        </>
+    )
 }
 
 export default ProductList
